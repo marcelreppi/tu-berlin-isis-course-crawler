957a7c5c0601e8433e99953fde507a3d103dabbd
Marcel Reppenhagen <marcelreppi@gmail.com>
2019-10-02 01:53:30 +0200

    update design, url check, resource scan

--- content_scripts/crawler.js
+++ content_scripts/crawler.js
@@ -1,11 +1,35 @@
-browser.runtime.onMessage.addListener(message => {
-  if (message.command === "crawl") {
-    document.querySelectorAll("a").forEach(node => {
-      if (node.href.startsWith("https://isis.tu-berlin.de/mod/resource/view.php?id=")) {
+if (!window.scriptHasRun) {
+  // Make sure script only runs once!
+  window.scriptHasRun = true
+
+  let resourceNodes = []
+
+  browser.runtime.onMessage.addListener(message => {
+    if (message.command === "scan") {
+      resourceNodes = []
+      document.querySelectorAll("a").forEach(node => {
+        if (node.href.startsWith("https://isis.tu-berlin.de/mod/resource/view.php?id=")) {
+          resourceNodes.push(node)
+        }
+      })
+
+      browser.runtime.sendMessage({
+        command: "scan-result",
+        numberOfResources: resourceNodes.length,
+      })
+
+      return
+    }
+
+    if (message.command === "crawl") {
+      resourceNodes.forEach(node => {
         fetch(node.href).then(res => {
+          // Content script can't access downloads API -> send msg to background script
           browser.runtime.sendMessage({ command: "download", url: res.url })
         })
-      }
-    })
-  }
-})
+      })
+
+      return
+    }
+  })
+}
--- popup/index.js
+++ popup/index.js
@@ -1,26 +1,43 @@
-function listenForClicks() {
-  document.addEventListener("click", e => {
-    function startCrawler(activeTab) {
+let activeTab = null
 
-      browser.tabs.sendMessage(activeTab.id, {
-        command: "crawl",
-      })
-    }
+function getActiveTab() {
+  return browser.tabs
+    .query({ active: true, currentWindow: true })
+    .then(tabs => {
+      activeTab = tabs[0]
+    })
+    .catch(reportError)
+}
 
-    function reportError(error) {
-      console.error(`Could not crawl: ${error}`)
-    }
+function checkURL() {
+  const { url } = activeTab
+  if (!url.startsWith("https://isis.tu-berlin.de/course/view.php?id=")) {
+    reportError(new Error("Not an ISIS course URL"))
+  }
+}
 
-    if (e.target.id === "crawl-button") {
-      browser.tabs
-        .query({ active: true, currentWindow: true })
-        .then(tabs => startCrawler(tabs[0]))
-        .catch(reportError)
-    }
+function scanForDocuments() {
+  browser.tabs.sendMessage(activeTab.id, {
+    command: "scan",
   })
 }
 
-function reportExecuteScriptError(error) {
+let preventCrawl = false
+function listenForCrawl() {
+  document.querySelector("#crawl-button").addEventListener("click", e => {
+    if (preventCrawl) return
+
+    document.querySelector("#crawl-button").classList.add("disabled")
+
+    browser.tabs.sendMessage(activeTab.id, {
+      command: "crawl",
+    })
+
+    preventCrawl = true
+  })
+}
+
+function reportError(error) {
   document.querySelector("#popup-content").classList.add("hidden")
   document.querySelector("#error-content").classList.remove("hidden")
   console.error(`Failed to execute crawling script: ${error.message}`)
@@ -28,7 +45,26 @@ function reportExecuteScriptError(error) {
 
 browser.tabs
   .executeScript({ file: "/content_scripts/crawler.js" })
-  .then(listenForClicks)
-  .catch(reportExecuteScriptError)
+  .then(getActiveTab)
+  .then(checkURL)
+  .then(scanForDocuments)
+  .then(listenForCrawl)
+  .catch(reportError)
 
+browser.runtime.onMessage.addListener(message => {
+  if (message.command === "scan-result") {
+    document.querySelector("#resource-info-loading").classList.add("hidden")
 
+    if (message.numberOfResources === 1) {
+      document.querySelector("#resource-info-single").classList.remove("hidden")
+    } else {
+      document.querySelector("#resource-info-number").textContent = message.numberOfResources
+      document.querySelector("#resource-info-multiple").classList.remove("hidden")
+
+      if (message.numberOfResources === 0) {
+        document.querySelector("#crawl-button").classList.add("disabled")
+        preventCrawl = true
+      }
+    }
+  }
+})
