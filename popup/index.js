436d833c7c28bd3afcd3333c2366da8b072b0343
Marcel Reppenhagen <marcelreppi@gmail.com>
2019-10-03 11:57:28 +0200

    add firefox dist, add browser polyfill, refactoring

--- popup/index.js
+++ popup/index.js
@@ -1,18 +1,17 @@
 let activeTab = null
-
 function getActiveTab() {
   return browser.tabs
     .query({ active: true, currentWindow: true })
     .then(tabs => {
       activeTab = tabs[0]
     })
-    .catch(reportError)
+    .catch(showErrorContent)
 }
 
 function checkURL() {
   const { url } = activeTab
   if (!url.startsWith("https://isis.tu-berlin.de/course/view.php?id=")) {
-    reportError(new Error("Not an ISIS course URL"))
+    showWrongPageContent()
   }
 }
 
@@ -38,19 +37,22 @@ function listenForCrawl() {
   })
 }
 
-function reportError(error) {
+function showWrongPageContent() {
+  document.querySelector("#popup-content").classList.add("hidden")
+  document.querySelector("#wrong-page-content").classList.remove("hidden")
+}
+
+function showErrorContent(error) {
   document.querySelector("#popup-content").classList.add("hidden")
   document.querySelector("#error-content").classList.remove("hidden")
   console.error(`Failed to execute crawling script: ${error.message}`)
 }
 
-browser.tabs
-  .executeScript({ file: "/content_scripts/crawler.js" })
-  .then(getActiveTab)
-  .then(checkURL)
-  .then(scanForDocuments)
-  .then(listenForCrawl)
-  .catch(reportError)
+getActiveTab().then(() => {
+  checkURL()
+  scanForDocuments()
+  listenForCrawl()
+})
 
 browser.runtime.onMessage.addListener(message => {
   if (message.command === "scan-result") {
