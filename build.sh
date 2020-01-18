82ebe20b0d3dc29a99b0013fbe29ec87d608ff80
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-01-18 19:54:32 +0100

    update build script

--- build.sh
+++ build.sh
@@ -2,3 +2,7 @@ npm run build
 
 cd build
 zip -r ../tu-berlin-isis-course-crawler .
+cd ..
+
+# Make zip of all the code for updating the extension
+zip -r ./tu-berlin-isis-course-crawler-code extension webpack.config.js package.json README.md
